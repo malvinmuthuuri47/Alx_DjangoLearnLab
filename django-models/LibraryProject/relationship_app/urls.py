79f7e51fe47476e3701afc11556e855e26684ab2
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-11-10 12:25:34 +0300

    Refactored Django views and url to DetailView as opposed to ListView

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import book_list, BookListView
 
 urlpatterns = [
-    path('books_fbv/', book_list, name='book-list'),
-    path('books_cbv/', BookListView.as_view(), name='book_list'),
+    path('books_fbv/', book_list, name='book_list_fbv'),
+    path('books_cbv/<int:pk>', BookListView.as_view(), name='book_list_cbv'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Book
 from .models import Library
 
@@ -8,7 +8,7 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-class BookListView(ListView):
+class BookListView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'libraries'   
+    context_object_name = 'library'
