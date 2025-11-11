a8c425297894c967a7cfc7cf72b3c3418e834b52
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-11-11 09:59:55 +0300

    Refactored the views for admin, librarian, and member by adding them to the relationship_app/views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from .models import Book
 from .models import Library
@@ -29,3 +30,42 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# Librarian logic
+def is_librarian(user):
+    """
+    This function checks for a user's session and the appropriate
+    role to access the view
+    """
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+@login_required
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member logic
+def is_member(user):
+    """
+    This function checks for a user's session and the appropriate
+    role to access the view
+    """
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+@login_required
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+# Admin logic
+def is_admin(user):
+    """
+    This function checks for a user's session and the appropriate
+    role to access the view
+    """
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+@login_required
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
