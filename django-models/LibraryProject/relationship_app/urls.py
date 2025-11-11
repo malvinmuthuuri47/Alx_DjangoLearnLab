a26fdee99dbdf9f39b07d12a8c61d5ddb5e38ea7
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-11-11 10:03:37 +0300

    Refactored the file structures and imports by removing redundant views for admin, librarian, and member

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import list_books
-from . import admin_view, librarian_view, member_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -11,7 +10,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin-dashboard/', admin_view.admin_view, name='admin_view'),
-    path('librarian-dashboard/', librarian_view.librarian_view, name='librarian_view'),
-    path('member-dashboard/', member_view.member_view, name='member_view'),
+    path('admin-dashboard/', views.admin_view, name='admin_view'),
+    path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
+    path('member-dashboard/', views.member_view, name='member_view'),
 ]
