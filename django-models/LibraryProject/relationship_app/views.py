0d94ac5739a881eaae237d3c39d2c3a112896b2c
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-11-11 12:35:59 +0300

    Added solution to the custom permissions in Django task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,72 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 from .models import Book
 from .models import Library
+from .models import Author
 
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+@csrf_exempt
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_id = request.POST.get('author_id')
+
+        if not title or not author_id:
+            return HttpResponse('Missing title or author_id', status=400)
+        
+        try:
+            author = Author.objects.get(id=author_id)
+        except Author.DoesNotExist:
+            return HttpResponse("Author not found", status=404)
+        
+        book = Book.objects.create(title=title, author=author)
+        return HttpResponse(f"Book '{book.title}' added successfully", status=201)
+    
+    return HttpResponse("Only Post method allowed", status=405)
+
+@csrf_exempt
+@permission_required('relationship_app.can_edit_book', raise_exception=True)
+def edit_book(request, pk):
+    if request.method == 'POST':
+        book = get_object_or_404(Book, pk=pk)
+        title = request.POST.get('title')
+        author_id = request.POST.get('author_id')
+
+        if title:
+            book.title = title
+        
+        if author_id:
+            try:
+                author = Author.objects.get(id=author_id)
+                book.author = author
+            except Author.DoesNotExist:
+                return HttpResponse("Author not found", status=404)
+        
+        book.save()
+        return HttpResponse(f"Book {book.id} updated successfully")
+    
+    return HttpResponse("Only POST method allowed", status=405)
+
+@csrf_exempt
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    if request.method == 'POST':
+        book = get_object_or_404(Book, pk=pk)
+        book.delete()
+        return HttpResponse(f"Book {pk} deleted successfully")
+    
+    return HttpResponse("Only POST method allowed", status=405)
 
 class LibraryDetailView(DetailView):
     model = Library
