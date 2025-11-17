91a81903da55179d5e60d7c30377a7907a651d33
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-11-17 10:48:51 +0300

    Refactored the admin site registration file to register the custom user

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title',)
     list_filter = ('title',)
 
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
