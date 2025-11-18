5d7d69451dd2796ad2d667ce54bc3c7251d0b0d4
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-11-18 10:33:00 +0300

    Updated the Django configurations for deployment, cookies, and Https

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -38,6 +38,19 @@ X_FRAME_OPTIONS = "DENY"
 # Only send cookies over HTTPS
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+CSRF_COOKIE_HTTPONLY = False
+
+# Redirect all HTTP -> HTTPS
+SECURE_SSL_REDIRECT = True
+
+# Enable HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# Protect deployement servers ensuring they use HTTPS
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Article
 
-class ExampleForm(forms.Modelform):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'body']
