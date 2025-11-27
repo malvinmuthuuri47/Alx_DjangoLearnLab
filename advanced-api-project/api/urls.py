23d15f54b84b437bac694f6f1cd7f9825ccde9a3
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-11-27 16:46:00 +0300

    Updated the urls.py and views.py files to use DRF generic views and the custom serializer created in task one

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+from .views import BookListAPI, BookDetailAPI, BookCreateAPI, BookUpdateAPI, BookDeleteAPI
 
 urlpatterns = [
-    path('listbooks/', BookListView.as_view(), name='book-list'),
-    path('info/<int:pk>/', BookDetailView.as_view(), name='book-info'),
-    path('books/create', BookCreateView.as_view(), name='book-create'),
-    path('books/update/<int:pk>', BookUpdateView.as_view(), name='book-update'),
-    path('books/delete/<int:pk>', BookDeleteView.as_view(), name='book-delete'),
+    path('listbooks/', BookListAPI.as_view()),
+    path('info/<int:pk>/', BookDetailAPI.as_view(), name='book-info'),
+    path('books/create/', BookCreateAPI.as_view(), name='book-create'),
+    path('books/update/<int:pk>', BookUpdateAPI.as_view(), name='book-update'),
+    path('books/delete/<int:pk>', BookDeleteAPI.as_view(), name='book-delete'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,51 +1,49 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .serializers import BookSerializer
 from .models import Book
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class BookListView(ListView):
+class BookListAPI(ListAPIView):
     """
     A class-based view to list all books in the database
     """
-    model = Book
-    template_name = 'api/book_list.html'
-    context_object_name = 'books'
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-class BookDetailView(DetailView):
+class BookDetailAPI(RetrieveAPIView):
     """
     A class-based view to show the details of a single book
     """
-    model = Book
-    template_name = 'api/book_detail.html'
-    context_object_name = 'book'
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
-class BookCreateView(LoginRequiredMixin, CreateView):
+class BookCreateAPI(ListCreateAPIView):
     """
     A class-based view to create a new book requiring a user
     to be logged in
     """
-    model = Book
-    fields = '__all__'
-    template_name = 'api/book_create.html'
-    success_url = reverse_lazy('book-list')
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
-class BookUpdateView(LoginRequiredMixin, UpdateView):
+class BookUpdateAPI(RetrieveUpdateAPIView):
     """
     A class-based view that updates a book instance requiring
     a user to be logged in
     """
-    model = Book
-    fields = ['title', 'author']
-    template_name = 'api/book_update.html'
-    success_url = reverse_lazy('book-list')
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
-class BookDeleteView(LoginRequiredMixin, DeleteView):
+
+class BookDeleteAPI(RetrieveDestroyAPIView):
     """
     A class-based view that deletes a book instance requiring
     a user to be logged in also
     """
-    model = Book
-    success_url = reverse_lazy('book-list')
-    template_name = 'api/book_delete.html'
-    context_object_name = 'book'
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
