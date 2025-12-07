02aa8d1e76ad101e3966831c1e8283c9f4ae8569
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-12-07 20:26:27 +0300

    Updated the blog/urls.py comment update path name as required

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
 
     # Comment urls
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
