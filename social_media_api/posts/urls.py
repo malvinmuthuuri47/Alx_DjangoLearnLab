c1ca60426899e3749517a06b63129c24f9a58ee9
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-12-14 21:07:09 +0300

    Updated posts/urls.py to include a feed url endpoint

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -8,4 +9,6 @@ router.register(r'posts', PostViewSet, basename='post')
 posts_router = NestedDefaultRouter(router, r'posts', lookup='post')
 posts_router.register(r'comments', CommentViewSet, basename='post-comments')
 
-urlpatterns = router.urls + posts_router.urls
+urlpatterns = router.urls + posts_router.urls + [
+    path('feed/', FeedView.as_view(), name='feed'),
+]
