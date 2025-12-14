14c89e5462171ab5a82d2828de7964fc1c8b7e47
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-12-14 03:12:36 +0300

    Updates posts/models.py to include models.TextField() for the content record in both models (Post and Comment)

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -5,13 +5,13 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, blank=True, null=True)
-    content = models.TextField(blank=True, null=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField(blank=True, null=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
